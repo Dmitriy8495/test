1ff606e9a7d4795234c88b44b7f7bd272eeca22d
Дмитрий Егорчев <dmitriy.egorchev@mail.ru>
2025-05-24 13:03:18 +0400

    me_skript/my_script

---
+++ me_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "git clone"
+echo "git branch"
+echo "git commit -m"
+echo "git checkout"
+echo "cat"
+echo "git show"
+echo "git pull"
+echo "git push"
+echo "git merge"
+echo "git log"
+echo "git status"
+echo "git add ."
+echo "git fetch"
+echo "git blame"
+echo "git diff"
+
